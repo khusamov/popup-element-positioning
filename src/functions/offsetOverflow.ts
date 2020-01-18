69be83b209eee6a86f1828781d1edfa1d2164e01
Khusamov Sukhrob <khusamov@yandex.ru>
2020-01-18 23:12:57 +0300

    Исправление offsetOverflow() под браузер Edge

--- src/functions/offsetOverflow.ts
+++ src/functions/offsetOverflow.ts
@@ -2,7 +2,8 @@
  * Вычисляет новые координаты прямоугольника с учетом компенсации его выхода за пределы экрана.
  */
 export default function offsetOverflow(element: Element, viewportWidth: number = document.documentElement.clientWidth, viewportHeight: number = document.documentElement.clientHeight) {
-	const {x, y, width, height} = element.getBoundingClientRect();
+	// Внимание, метод getBoundingClientRect() возвращает также x и y, которые не доступны в браузере Edge, поэтому тут вместо них используются left и top.
+	const {left: x, top: y, width, height} = element.getBoundingClientRect();
 
 	const offsetX = x + width - viewportWidth;
 	const offsetY = y + height - viewportHeight;
